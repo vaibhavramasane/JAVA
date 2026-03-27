2e46c7fda152b96355bd4c6133f50977312216d2
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-27 09:42:27 +0530

    first commit

--- DecisionMaking/Q9.java
+++ DecisionMaking/Q9.java
@@ -7,3 +7,27 @@ Vowel
 
 Explanation:
 Vowels: a, e, i, o, u.*/
+
+import java.util.*;
+
+class Q9{
+
+	public static void main(String x[]){
+
+		Scanner xyz = new Scanner(System.in);
+		System.out.println("Enter a character: ");
+		char ch = xyz.next().charAt(0);
+
+		if(ch=='a'|| ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='O' || ch=='o' || ch=='u' || ch=='U'){
+			System.out.println("It is vowels");
+		}else{
+			System.out.println("it is an consonant");
+
+		}
+
+	}
+}
+
+		
+
+		
