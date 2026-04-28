dd2aed282911c9720f3cd821973dd1e95971d1ce
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-04-28 12:42:47 +0530

    first comment

--- String/Q3.java
+++ String/Q3.java
@@ -5,3 +5,23 @@ Example:
 Java programming language 
 Longest word → programming 
 Output: programming  */
+
+import java.util.*;
+import java.io.*;
+
+class Q3{
+	
+	public static void main(String x[]) throws IOException{
+		
+		BufferedWriter br = new BufferedWriter(new FileWriter("data.txt"));
+		
+		Scanner xyz = new Scanner(br);
+		System.out.println("Enter Input: ");
+		String data = xyz.nextLine();
+		
+		br.write(data);
+		xyz.nextLine();
+		System.out.println("save sucessfully:");
+		br.close();
+	}
+}
